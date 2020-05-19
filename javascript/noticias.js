98f8f12ec805ee4ce1c6a9e53ec3bd3308531d5c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-05-19 20:48:15 -0300

    Technology news and update button

--- javascript/noticias.js
+++ javascript/noticias.js
@@ -1,18 +1,17 @@
 const API_KEY = 'c2bb51d7b6f74baa879b2d1ae7bafc43'
 const BASE_API = 'https://newsapi.org/v2'
 
-const divListaNoticias = document.querySelector('#listaDeNoticias')
+const divNewsList = document.querySelector('#newsList')
+const btnUpdate = document.getElementById('btnUpdate')
+const linkLastNews = document.getElementById('lastNews')
+const linkTechNews = document.getElementById('techNews')
 
-let articlesList = ''
+let activeId = ''
 
 
-// function getNoticias() {
-//   fetch(`${BASE_API}/top-headlines?country=br&apiKey=${API_KEY}`)
-//     .then(response => response.json())
-//     .then(newsList => console.log(newsList))
-// }
+async function getLastNews() {
+  clearArticles()
 
-async function getNoticias() {
   const resposta = await fetch(
     `${BASE_API}/top-headlines?country=br&apiKey=${API_KEY}`
   )
@@ -22,8 +21,20 @@ async function getNoticias() {
   buildArticles(newsList.articles)
 }
 
+async function getTechNews() {
+  clearArticles()
+
+  const resposta = await fetch(
+    `${BASE_API}/top-headlines?country=br&category=technology&apiKey=${API_KEY}`
+  )
+
+  const newsList = await resposta.json()
+
+  buildArticles(newsList.articles)
+}
+
 function buildArticles(articles) {
-  console.log(articles)
+
   articles.forEach(article => {
     let column = document.createElement('div')
     column.classList.add('col-4')
@@ -63,8 +74,52 @@ function buildArticles(articles) {
 
     cardBody.append(link)
 
-    divListaNoticias.append(column)
+    divNewsList.append(column)
   })
 }
 
-getNoticias()
+function clearArticles() {
+  divNewsList.innerHTML = ''
+}
+
+function toggleActive(event) {
+  document.querySelector('.active').classList.remove('active')
+  event.target.parentNode.classList.toggle('active')
+}
+
+btnUpdate.addEventListener('click', event => {
+  clearArticles()
+
+  activeId = document.querySelector('.active')
+    .firstChild.nextElementSibling.id
+
+  switch (activeId) {
+    case 'lastNews':
+      getLastNews()
+      break
+    case 'techNews':
+      getTechNews()
+      break
+  }
+})
+
+linkLastNews.addEventListener('click', event => {
+  clearArticles()
+
+  toggleActive(event)
+
+  getLastNews()
+})
+
+linkTechNews.addEventListener('click', event => {
+  clearArticles()
+
+  toggleActive(event)
+
+  getTechNews()
+})
+
+// get News on page load
+window.onload = () => {
+  getLastNews()
+}
